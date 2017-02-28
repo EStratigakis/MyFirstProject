dbe25c6425acbebf77dfc2db6c51ea0c7bac9501
EStratigakis <strpao13@hotmail.com>
2017-02-28 15:50:51 +0000

    HTML first try

---
+++ test.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+
+
+<html>
+<head>
+
+</head>
+<body>
+<p>
+    <?php
+    echo "Hello World";
+    $myname = "Frodo Baggins";
+    $myage = 111;
+    echo "My name is" . $myname . "and I am " . $myage;
+    ?>
+</body>
+</html>
