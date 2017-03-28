48142f403cfa9a8671a9e90dd9c2fa5f8a83b12c
EStratigakis <e.stratigakis@rgu.ac.uk>
2017-03-28 21:13:01 +0300

    hi

--- index.php
+++ index.php
@@ -10,8 +10,22 @@
     <?php
     echo "Hello World";
     $myname = "Frodo Baggins";
-    $myage = 111;
+    $myage = 17;
     echo "My name is " . $myname . "and I am " . $myage;
-    ?>
+
+    if ($myage >= 16){
+        print "You can buy specs!";
+        if ($myage >= 18){
+            print "You can buy mugs";
+            if ($myage >= 21){
+                print "You can buy sausage rolls";
+            }
+        }
+    }
+    else {
+        print "You cannot buy anything!";
+    }
+
+   ?>
 </body>
 </html>
