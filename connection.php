ea9423d43270c3575dcd99b1557361d415c43538
EStratigakis <e.stratigakis@rgu.ac.uk>
2017-03-30 22:05:07 +0300

    hi

--- connection.php
+++ connection.php
@@ -4,4 +4,7 @@ define('DB_USERNAME', 'azure');
 define('DB_PASSWORD', '');
 define('DB_DATABASE', 'user');
 $link = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+if ($link) {
+    echo 'OK';
+}
 ?>
