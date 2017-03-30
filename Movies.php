12459eed8d13d3c43fe2c2dc0e926891d842cab9
EStratigakis <e.stratigakis@rgu.ac.uk>
2017-03-30 19:13:56 +0300

    hi

--- Movies.php
+++ Movies.php
@@ -7,15 +7,15 @@
 <body>
     <?php
     include 'dbConnect.php';
-    mysqli_select_db($link,'marvelmovie');
-    $sql= "SELECT * FROM marvelmovies";
-    $result=mysqli_query($link,$sql);
-        while($row = $result ->fetch_array())
+    mysqli_select_db($link,'marvelmovie'); /* selects the database that is going to used and uses the connection in dbConnet.php */
+    $sql= "SELECT * FROM marvelmovies"; /* this is the sql statement to select everything in the database*/
+    $result=mysqli_query($link,$sql); /*this stores the statement in a string*/
+        while($row = $result ->fetch_array())/*runs through the entries in the database one time and puts them in an array*/
         {
         /* the code inside here is repeated for each item in the array
         You can do things like the following to print out each movie title */
-        $movieTitle = $row['title'];
-        echo '<tr><td>' . $movieTitle .'</td></tr>';
+        $movieTitle = $row['title'];/*gets the title of the movie and puts it a string*/
+        echo '<tr>' . $movieTitle .'</tr>';/* prints the movie title to the end user*/
 
         }
     ?>
