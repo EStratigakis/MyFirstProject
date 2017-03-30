6f5e891ea021f00f93ed5158d2d81081c25d813e
EStratigakis <e.stratigakis@rgu.ac.uk>
2017-03-30 21:49:08 +0300

    hi

--- connection.php
+++ connection.php
@@ -1,21 +1,7 @@
 <?php
-
-// this will avoid mysql_connect() deprecation error.
-error_reporting( ~E_DEPRECATED & ~E_NOTICE );
-// but I strongly suggest you to use PDO or MySQLi.
-
-define('DBHOST', 'localhost');
-define('DBUSER', 'root');
-define('DBPASS', '');
-define('DBNAME', 'user');
-
-$conn = mysqli_connect(DBHOST,DBUSER,DBPASS);
-$dbcon = mysqli_select_db(DBNAME);
-
-if ( !$conn ) {
-    die("Connection failed : " . mysqli_error());
-}
-
-if ( !$dbcon ) {
-    die("Database Connection failed : " . mysqli_error());
-}
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'user');
+$db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+?>
