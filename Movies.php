d3095b0fc540999ad5f1c9434cfc2a1dc3481fb2
EStratigakis <e.stratigakis@rgu.ac.uk>
2017-03-30 17:28:29 +0300

    hi

--- Movies.php
+++ Movies.php
@@ -1,13 +1,23 @@
-<?php
-include ("dbConnect.php");
-$sql_query = "SELECT * FROM marvelmovies";
-$result=mysqli_query($db,$sql);
-while($row = $result->fetch_array())
-{
-    /* the code inside here is repeated for each item in the array
-    You can do things like the following to print out each movie title */
+<!DOCTYPE html>
 
-    $movieTitle = $row[‘title’];
-    echo “<p>” . $movieTitle . “</p>”;
+<html>
+<head>
 
-}
+</head>
+<body>
+    <?php
+    include "dbConnect.php.";
+    $sql_query = "SELECT * FROM marvelmovies";
+    $result=mysqli_query($db,$sql);
+        while($row = $result->fetch_array())
+        {
+        /* the code inside here is repeated for each item in the array
+        You can do things like the following to print out each movie title */
+
+        $movieTitle = $row;
+        echo $movieTitle;
+
+        }
+    ?>
+</body>
+</html>
