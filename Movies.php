75bed53a31460f117118159bc7d23cb1947aec51
EStratigakis <e.stratigakis@rgu.ac.uk>
2017-03-30 17:59:58 +0300

    hi

--- Movies.php
+++ Movies.php
@@ -1,12 +1,14 @@
 <!DOCTYPE html>
-
+<?php
+include "dbConnect.php.";
+?>
 <html>
 <head>
+<title> Hello. Let's hope it works!!!</title>
 
 </head>
 <body>
     <?php
-    include "dbConnect.php.";
     $sql_query = "SELECT * FROM 'marvelmovies'";
     $result=sql_query($db,$sql);
         while($row = $result->fetch_array())
@@ -14,7 +16,7 @@
         /* the code inside here is repeated for each item in the array
         You can do things like the following to print out each movie title */
 
-        echo $row[‘title’];
+        echo $row;
 
         }
     ?>
