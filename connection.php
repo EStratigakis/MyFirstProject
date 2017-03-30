3e8368117b4a091c82b6d06270c871ea557e41ec
EStratigakis <e.stratigakis@rgu.ac.uk>
2017-03-30 21:50:54 +0300

    hi

--- connection.php
+++ connection.php
@@ -3,5 +3,5 @@ define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
 define('DB_DATABASE', 'user');
-$db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+$link = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
 ?>
