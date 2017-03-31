5d28ef3e47b80df2b6e7c4551e7ef1645b7de964
EStratigakis <e.stratigakis@rgu.ac.uk>
2017-03-31 17:35:42 +0300

    Revert "hi"
    This reverts commit a4bd17b76f5e671bd77252e18151937de541b5ed.

--- insertSuperhero.php
+++ insertSuperhero.php
@@ -1,14 +1,14 @@
 <?php
 
 include ("connection.php");
-
+mysqli_select_db($link,'superbattles');
 $firstname = $_POST["firstname"];
 $lastname = $_POST["lastname"];
 $superpower = $_POST["superpower"];
-mysqli_select_db($link,'superbattles');
+
 $sql = "INSERT INTO superheros (firstName, lastName, mainSuperpower) VALUES ('$firstname','$lastname','$superpower')";
 
-if (mysqli_query($link, $sql)) {echo "ok";}
+if (mysqli_query($link, $sql)) {}
 else {
     echo "Error: " . $sql . "<br>" . mysqli_error($link);
 }
