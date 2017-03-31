11df75a7213cb7e19073a3f1687c9f9eeb6ad559
EStratigakis <e.stratigakis@rgu.ac.uk>
2017-03-31 17:00:51 +0300

    hi

--- insertSuperhero.php
+++ insertSuperhero.php
@@ -8,7 +8,7 @@ $superpower = $_POST["superpower"];
 mysqli_select_db($link,'superbattles');
 $sql = "INSERT INTO superheros (firstName, lastName, mainSuperpower) VALUES ('$firstname','$lastname','$superpower')";
 
-if (mysqli_query($link, $sql)) {}
+if (mysqli_query($link, $sql)) {echo "ok";}
 else {
     echo "Error: " . $sql . "<br>" . mysqli_error($link);
 }
