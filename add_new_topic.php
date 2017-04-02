8097fabac3bd379d2baa1b66ede677e14b76e87b
EStratigakis <e.stratigakis@rgu.ac.uk>
2017-04-02 22:57:29 +0300

    hi

---
+++ add_new_topic.php
@@ -0,0 +1,22 @@
+<?php
+include ("dbConnect.php");
+// get data that sent from form
+$topic=$_POST['topic'];
+$detail=$_POST['detail'];
+$name=$_POST['name'];
+$email=$_POST['email'];
+
+$datetime=date("d/m/y h:i:s"); //create date time
+
+$sql="INSERT INTO $tbl_name(topic, detail, name, email, datetime)VALUES('$topic', '$detail', '$name', '$email', '$datetime')";
+$result=mysqli_query($sql);
+
+if($result){
+    echo "Successful<BR>";
+    echo "<a href=main_forum.php>View your topic</a>";
+}
+else {
+    echo "ERROR";
+}
+mysqli_close();
+?>
