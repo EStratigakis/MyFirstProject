2157eac6da2965c91ce92c8aa04e7679a8472e19
EStratigakis <e.stratigakis@rgu.ac.uk>
2017-04-08 15:20:50 +0100

    hi

--- index.php
+++ index.php
@@ -2,16 +2,72 @@
 <html>
 <head>
     <meta charset="utf-8">
-    <title>PHP Login Form without Session</title>
+    <title>Robert Gordon University</title>
     <link rel="stylesheet" href="style.css" type="text/css" />
 </head>
 
 <body>
-<h1>PHP Login Form with Session</h1>
+<h1>RGU Login</h1>
 <div class="loginBox">
-    <h3>Login Form</h3>
+    <h3>Login
+        <style>
+            body {
+                padding-top: 40px;
+                padding-bottom: 40px;
+                background-color: #ADABAB;
+            }
+
+            .form-signin {
+                max-width: 330px;
+                padding: 15px;
+                margin: 0 auto;
+                color: #017572;
+            }
+
+            .form-signin .form-signin-heading,
+            .form-signin .checkbox {
+                margin-bottom: 10px;
+            }
+
+            .form-signin .checkbox {
+                font-weight: normal;
+            }
+
+            .form-signin .form-control {
+                position: relative;
+                height: auto;
+                -webkit-box-sizing: border-box;
+                -moz-box-sizing: border-box;
+                box-sizing: border-box;
+                padding: 10px;
+                font-size: 16px;
+            }
+
+            .form-signin .form-control:focus {
+                z-index: 2;
+            }
+
+            .form-signin input[type="email"] {
+                margin-bottom: -1px;
+                border-bottom-right-radius: 0;
+                border-bottom-left-radius: 0;
+                border-color:#017572;
+            }
+
+            .form-signin input[type="password"] {
+                margin-bottom: 10px;
+                border-top-left-radius: 0;
+                border-top-right-radius: 0;
+                border-color:#017572;
+            }
+
+            h2{
+                text-align: center;
+                color: #017572;
+            }
+        </style>
     <br><br>
-    <form method="post" action="login.php">
+    <form class = "form-signin" role = "form" method="post" action="login.php">
         <label>Username:</label><br>
         <input type="text" name="username" placeholder="username" /><br><br>
         <label>Password:</label><br>
