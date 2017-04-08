0405878c33b03fefa83bc90a4f76b89860016c63
EStratigakis <e.stratigakis@rgu.ac.uk>
2017-04-08 18:56:43 +0100

    hi

--- Login System/login1.php
+++ Login System/login1.php
@@ -27,3 +27,50 @@ if(empty($_POST["username"]) || empty($_POST["password"]))
 }
 
 ?>
+<!DOCTYPE html>
+
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Creating a New Topic!</title>
+    <link rel="stylesheet" href="/style.css">
+    <link rel="stylesheet" href="/assets/CSS/unsemantic-grid-responsive-tablet.css">
+    <style>
+        body {margin:0;}
+
+        .topnav {
+            overflow: hidden;
+            background-color: #333;
+        }
+
+        .topnav a {
+            float: left;
+            display: block;
+            color: #f2f2f2;
+            text-align: center;
+            padding: 14px 16px;
+            text-decoration: none;
+            font-size: 17px;
+        }
+
+        .topnav a:hover {
+            background-color: #ddd;
+            color: black;
+        }
+
+        .topnav a.active {
+            background-color: rebeccapurple;
+            color: white;
+        }
+    </style>
+</head>
+<body>
+<link rel="stylesheet" href="/style.css">
+<div class="topnav">
+    <a class="active" href="/index.php">Home</a>
+    <a href="/Forum/new_topic.php">Create a New topic</a>
+    <a href="/Forum/main_forum.php">Go to Main Forum</a>
+    <a href="/Login%20System/login.php">Login</a>
+</div>
+</body>
+</html>
