c7a966266688d6a58e958514a5bd565fc9a26af7
EStratigakis <e.stratigakis@rgu.ac.uk>
2017-04-08 22:14:48 +0100

    hi

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <title>Robert Gordon University</title>
     <link rel="stylesheet" href="style.css" type="text/css" />
         <style>
@@ -46,7 +47,7 @@
                 width: 50px;
                 text-align: center;
                 text-decoration: none;
-                margin: 5px 2px;
+                border-radius: 50%;
             }
 
             .fa:hover {
