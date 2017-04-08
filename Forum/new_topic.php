06da9fa60796834148a512905af89075dc44e9e6
EStratigakis <e.stratigakis@rgu.ac.uk>
2017-04-08 18:25:29 +0100

    hi

--- Forum/new_topic.php
+++ Forum/new_topic.php
@@ -4,10 +4,18 @@
 <head>
     <meta charset="UTF-8">
     <title>Creating a New Topic!</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="/style.css">
     <link rel="stylesheet" href="assets/CSS/unsemantic-grid-responsive-tablet.css">
 </head>
 <body>
+<link rel="stylesheet" href="/style.css">
+<div class="topnav">
+    <a class="active" href="/index.php">Home</a>
+    <a href="/Forum/new_topic.php">Create a New topic</a>
+    <a href="/Forum/main_forum.php">Go to Main Forum</a>
+    <a href="/Login%20System/login.php">Login</a>
+</div>
+
 <table width="400" border="0" align="center" cellpadding="0" cellspacing="1" bgcolor="#CCCCCC">
     <tr>
         <form id="form1" name="form1" method="post" action="add_new_topic.php">
