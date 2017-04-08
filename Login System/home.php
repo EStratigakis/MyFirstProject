01dec3ebd98aa4a29023976e32f2fa59414127c3
EStratigakis <e.stratigakis@rgu.ac.uk>
2017-04-08 17:38:31 +0100

    hi

--- Login System/home.php
+++ Login System/home.php
@@ -7,6 +7,14 @@
 </head>
 
 <body>
+
+<div class="topnav">
+    <a class="active" href="/index.php">Home</a>
+    <a href="/Forum/new_topic.php">Create a New topic</a>
+    <a href="/Forum/main_forum.php">Go to Main Forum</a>
+    <a href="/Login%20System/login.php">Login</a>
+</div>
+
 <h1>Hello</h1>
 </body>
 <footer>
