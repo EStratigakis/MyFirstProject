c9e92edeec17fbc7547730fb8fa9212a69b9922f
EStratigakis <e.stratigakis@rgu.ac.uk>
2017-04-08 17:13:38 +0100

    hi

--- index.php
+++ index.php
@@ -51,11 +51,15 @@
 </head>
 
 <body>
+
 <div class="topnav">
     <a class="active" href="/index.php">Home</a>
     <a href="Forum/new_topic.php">Create a New topic</a>
     <a href="Forum/main_forum.php">Go to Main Forum</a>
     <a href="Login%20System/login.php">Login</a>
 </div>
+
+<div class="bg"></div>
+
 </body>
 </html>
