bebc01c555914589c842bc091a8d182e00757a24
EStratigakis <e.stratigakis@rgu.ac.uk>
2017-04-08 14:38:11 +0100

    hi

--- index.php
+++ index.php
@@ -1,150 +1,62 @@
 <?php
-if(!empty($_POST["register-user"])) {
-	/* Form Required Field Validation */
-	foreach($_POST as $key=>$value) {
-		if(empty($_POST[$key])) {
-		$error_message = "All Fields are required";
-		break;
-		}
-	}
-	/* Password Matching Validation */
-	if($_POST['password'] != $_POST['confirm_password']){ 
-	$error_message = 'Passwords should be same<br>'; 
-	}
+require('dbConnect.php');
+// If the values are posted, insert them into the database.
+if (isset($_POST['username']) && isset($_POST['password'])){
+    $username = $_POST['username'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
 
-	/* Email Validation */
-	if(!isset($error_message)) {
-		if (!filter_var($_POST["userEmail"], FILTER_VALIDATE_EMAIL)) {
-		$error_message = "Invalid Email Address";
-		}
-	}
-
-	/* Validation to check if gender is selected */
-	if(!isset($error_message)) {
-	if(!isset($_POST["gender"])) {
-	$error_message = " All Fields are required";
-	}
-	}
-
-	/* Validation to check if Terms and Conditions are accepted */
-	if(!isset($error_message)) {
-		if(!isset($_POST["terms"])) {
-		$error_message = "Accept Terms and Conditions to Register";
-		}
-	}
-
-	if(!isset($error_message)) {
-		require_once("dbConnect.php");
-        mysqli_select_db($db,"losystem");
-		$query = "INSERT INTO registered_users (user_name, first_name, last_name, password, email, gender) VALUES
-		('" . $_POST["userName"] . "', '" . $_POST["firstName"] . "', '" . $_POST["lastName"] . "', '" . md5($_POST["password"]) . "', '" . $_POST["userEmail"] . "', '" . $_POST["gender"] . "')";
-		$result = $db->insertQuery($query);
-		if(!empty($result)) {
-			$error_message = "";
-			$success_message = "You have registered successfully!";	
-			unset($_POST);
-		} else {
-			$error_message = "Problem in registration. Try Again!";
-		}
-	}
+    $query = "INSERT INTO `user` (username, password, email) VALUES ('$username', '$password', '$email')";
+    $result = mysqli_query($db, $query);
+    if($result){
+        $smsg = "User Created Successfully.";
+    }else{
+        $fmsg ="User Registration Failed";
+    }
 }
 ?>
 <html>
 <head>
-<title>PHP! User Registration Form</title>
-<style>
-body{
-	width:610px;
-	font-family:calibri;
-}
-.error-message {
-	padding: 7px 10px;
-	background: #fff1f2;
-	border: #ffd5da 1px solid;
-	color: #d6001c;
-	border-radius: 4px;
-}
-.success-message {
-	padding: 7px 10px;
-	background: #cae0c4;
-	border: #c3d0b5 1px solid;
-	color: #027506;
-	border-radius: 4px;
-}
-.demo-table {
-	background: #d9eeff;
-	width: 100%;
-	border-spacing: initial;
-	margin: 2px 0px;
-	word-break: break-all;
-	table-layout: auto;
-	line-height: 1.8em;
-	color: #333;
-	border-radius: 4px;
-	padding: 20px 40px;
-}
-.demo-table td {
-	padding: 15px 0px;
-}
-.demoInputBox {
-	padding: 10px 30px;
-	border: #a9a9a9 1px solid;
-	border-radius: 4px;
-}
-.btnRegister {
-	padding: 10px 30px;
-	background-color: #3367b2;
-	border: 0;
-	color: #FFF;
-	cursor: pointer;
-	border-radius: 4px;
-	margin-left: 10px;
-}
-</style>
+    <title>User Registeration Using PHP & MySQL</title>
+
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" >
+
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" >
+
+    <link rel="stylesheet" href="style.css" >
+
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </head>
 <body>
-<form name="frmRegistration" method="post" action="">
-<table border="0" width="500" align="center" class="demo-table">
-<?php if(!empty($success_message)) { ?>	
-<div class="success-message"><?php if(isset($success_message)) echo $success_message; ?></div>
-<?php } ?>
-<?php if(!empty($error_message)) { ?>	
-<div class="error-message"><?php if(isset($error_message)) echo $error_message; ?></div>
-<?php } ?>
-<tr>
-<td>User Name</td>
-<td><input type="text" class="demoInputBox" name="userName" value="<?php if(isset($_POST['userName'])) echo $_POST['userName']; ?>"></td>
-</tr>
-<tr>
-<td>First Name</td>
-<td><input type="text" class="demoInputBox" name="firstName" value="<?php if(isset($_POST['firstName'])) echo $_POST['firstName']; ?>"></td>
-</tr>
-<tr>
-<td>Last Name</td>
-<td><input type="text" class="demoInputBox" name="lastName" value="<?php if(isset($_POST['lastName'])) echo $_POST['lastName']; ?>"></td>
-</tr>
-<tr>
-<td>Password</td>
-<td><input type="password" class="demoInputBox" name="password" value=""></td>
-</tr>
-<tr>
-<td>Confirm Password</td>
-<td><input type="password" class="demoInputBox" name="confirm_password" value=""></td>
-</tr>
-<tr>
-<td>Email</td>
-<td><input type="text" class="demoInputBox" name="userEmail" value="<?php if(isset($_POST['userEmail'])) echo $_POST['userEmail']; ?>"></td>
-</tr>
-<tr>
-<td>Gender</td>
-<td><input type="radio" name="gender" value="Male" <?php if(isset($_POST['gender']) && $_POST['gender']=="Male") { ?>checked<?php  } ?>> Male
-<input type="radio" name="gender" value="Female" <?php if(isset($_POST['gender']) && $_POST['gender']=="Female") { ?>checked<?php  } ?>> Female
-</td>
-</tr>
-<tr>
-<td colspan=2>
-<input type="checkbox" name="terms"> I accept Terms and Conditions <input type="submit" name="register-user" value="Register" class="btnRegister"></td>
-</tr>
-</table>
-</form>
-</body></html>
+
+<div class="container">
+    <form class="form-signin" method="POST">
+
+        <?php if(isset($smsg)){ ?><div class="alert alert-success" role="alert"> <?php echo $smsg; ?> </div><?php } ?>
+        <?php if(isset($fmsg)){ ?><div class="alert alert-danger" role="alert"> <?php echo $fmsg; ?> </div><?php } ?>
+        <h2 class="form-signin-heading">Please Register</h2>
+        <div class="input-group">
+            <span class="input-group-addon" id="basic-addon1">@</span>
+            <input type="text" name="username" class="form-control" placeholder="Username" required>
+        </div>
+        <label for="inputEmail" class="sr-only">Email address</label>
+        <input type="email" name="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
+        <label for="inputPassword" class="sr-only">Password</label>
+        <input type="password" name="password" id="inputPassword" class="form-control" placeholder="Password" required>
+        <div class="checkbox">
+            <label>
+                <input type="checkbox" value="remember-me"> Remember me
+            </label>
+        </div>
+        <button class="btn btn-lg btn-primary btn-block" type="submit">Register</button>
+        <a class="btn btn-lg btn-primary btn-block" href="login.php">Login</a>
+    </form>
+</div>
+
+</body>
+
+</html>
+</html>
