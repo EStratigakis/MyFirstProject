57de7906e0eab9eccb36eb8f7bf1377e59fcd9aa
EStratigakis <e.stratigakis@rgu.ac.uk>
2017-04-08 18:58:23 +0100

    hi

--- Login System/login.php
+++ Login System/login.php
@@ -3,94 +3,101 @@
 <html>
 <head>
     <title>Login</title>
+    <style>
+        body {margin:0;}
+
+        .topnav {
+            overflow: hidden;
+            background-color: #333;
+        }
+
+        .topnav a {
+            float: left;
+            display: block;
+            color: #f2f2f2;
+            text-align: center;
+            padding: 14px 16px;
+            text-decoration: none;
+            font-size: 17px;
+        }
+
+        .topnav a:hover {
+            background-color: #ddd;
+            color: black;
+        }
+
+        .topnav a.active {
+            background-color: rebeccapurple;
+            color: white;
+        }
+        body {
+            padding-top: 40px;
+            padding-bottom: 40px;
+            background-color: white;
+        }
+
+        .form-signin {
+            max-width: 330px;
+            padding: 15px;
+            margin: 0 auto;
+            color: rebeccapurple;
+        }
+
+        .form-signin .form-signin-heading,
+        .form-signin .checkbox {
+            margin-bottom: 10px;
+        }
+
+        .form-signin .checkbox {
+            font-weight: normal;
+        }
+
+        .form-signin .form-control {
+            position: relative;
+            height: auto;
+            -webkit-box-sizing: border-box;
+            -moz-box-sizing: border-box;
+            box-sizing: border-box;
+            padding: 10px;
+            font-size: 16px;
+        }
+
+        .form-signin .form-control:focus {
+            z-index: 2;
+        }
+
+        .form-signin input[type="email"] {
+            margin-bottom: -1px;
+            border-bottom-right-radius: 0;
+            border-bottom-left-radius: 0;
+            border-color:#017572;
+        }
+
+        .form-signin input[type="password"] {
+            margin-bottom: 10px;
+            border-top-left-radius: 0;
+            border-top-right-radius: 0;
+            border-color:#017572;
+        }
+
+        title{
+            text-align: center;
+            color: #017572;
+        }
+    </style>
 </head>
 <body>
-
+<link rel="stylesheet" href="/style.css">
+<div class="topnav">
+    <a class="active" href="/index.php">Home</a>
+    <a href="/Forum/new_topic.php">Create a New topic</a>
+    <a href="/Forum/main_forum.php">Go to Main Forum</a>
+    <a href="/Login%20System/login.php">Login</a>
+</div>
 <div class="container form-signin">
     <h1>RGU Login</h1>
     <h3>Login
-        <style>
-            body {margin:0;}
 
-            .topnav {
-                overflow: hidden;
-                background-color: #333;
-            }
-
-            .topnav a {
-                float: left;
-                display: block;
-                color: #f2f2f2;
-                text-align: center;
-                padding: 14px 16px;
-                text-decoration: none;
-                font-size: 17px;
-            }
-
-            .topnav a:hover {
-                background-color: #ddd;
-                color: black;
-            }
-
-            .topnav a.active {
-                background-color: rebeccapurple;
-                color: white;
-            }
-            body {
-                padding-top: 40px;
-                padding-bottom: 40px;
-                background-color: white;
-            }
-
-            .form-signin {
-                max-width: 330px;
-                padding: 15px;
-                margin: 0 auto;
-                color: rebeccapurple;
-            }
-
-            .form-signin .form-signin-heading,
-            .form-signin .checkbox {
-                margin-bottom: 10px;
-            }
-
-            .form-signin .checkbox {
-                font-weight: normal;
-            }
-
-            .form-signin .form-control {
-                position: relative;
-                height: auto;
-                -webkit-box-sizing: border-box;
-                -moz-box-sizing: border-box;
-                box-sizing: border-box;
-                padding: 10px;
-                font-size: 16px;
-            }
-
-            .form-signin .form-control:focus {
-                z-index: 2;
-            }
-
-            .form-signin input[type="email"] {
-                margin-bottom: -1px;
-                border-bottom-right-radius: 0;
-                border-bottom-left-radius: 0;
-                border-color:#017572;
-            }
-
-            .form-signin input[type="password"] {
-                margin-bottom: 10px;
-                border-top-left-radius: 0;
-                border-top-right-radius: 0;
-                border-color:#017572;
-            }
-
-            title{
-                text-align: center;
-                color: #017572;
-            }
-        </style>
         <br><br>
         <form class = "form-signin" role = "form" method="post" action="login1.php">
             <label>Username:</label><br>
@@ -100,13 +107,6 @@
             <input type="submit" name="submit" value = "login"/>
         </form>
         <div class="error"><?php //echo $error;?><?php //echo $username; echo $password;?></div>
-
-        <div class="topnav">
-            <a class="active" href="/index.php">Home</a>
-            <a href="/Forum/new_topic.php">Create a New topic</a>
-            <a href="/Forum/main_forum.php">Go to Main Forum</a>
-            <a href="/Login%20System/login.php">Login</a>
-        </div>
 
 </body>
 <footer>
