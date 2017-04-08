be4918ae6985eefe8ed070295da71b67d46d30a5
EStratigakis <e.stratigakis@rgu.ac.uk>
2017-04-08 13:59:15 +0100

    hi

--- index.php
+++ index.php
@@ -38,14 +38,14 @@ if(!empty($_POST["register-user"])) {
 		mysqli_select_db($db,"losystem");
 		$query = "INSERT INTO registered_users (user_name, first_name, last_name, password, email, gender) VALUES
 		('" . $_POST["userName"] . "', '" . $_POST["firstName"] . "', '" . $_POST["lastName"] . "', '" . md5($_POST["password"]) . "', '" . $_POST["userEmail"] . "', '" . $_POST["gender"] . "')";
-		$result = mysqli_query($db, $query);
+		$result = mysqli_query($query);
 		if($result) {
 			$error_message = "";
 			$success_message = "You have registered successfully!";	
 			unset($_POST);
 		} else {
 			$error_message = "Problem in registration. Try Again!";
-			echo "im here!!";
+			echo "im here!";
 		}
 	}
 }
