2b92687f17a343571898d810124bc821e987b7ca
EStratigakis <e.stratigakis@rgu.ac.uk>
2017-04-08 18:55:40 +0100

    hi

--- Forum/new_topic.php
+++ Forum/new_topic.php
@@ -6,7 +6,34 @@
     <title>Creating a New Topic!</title>
     <link rel="stylesheet" href="/style.css">
     <link rel="stylesheet" href="/assets/CSS/unsemantic-grid-responsive-tablet.css">
+<style>
+    body {margin:0;}
 
+    .topnav {
+    overflow: hidden;
+    background-color: #333;
+    }
+
+    .topnav a {
+    float: left;
+    display: block;
+    color: #f2f2f2;
+    text-align: center;
+    padding: 14px 16px;
+    text-decoration: none;
+    font-size: 17px;
+    }
+
+    .topnav a:hover {
+    background-color: #ddd;
+    color: black;
+    }
+
+    .topnav a.active {
+    background-color: rebeccapurple;
+    color: white;
+    }
+</style>
 
 </head>
 <body>
--- Login System/login.php
+++ Login System/login.php
@@ -10,6 +10,32 @@
     <h1>RGU Login</h1>
     <h3>Login
         <style>
+            body {margin:0;}
+
+            .topnav {
+                overflow: hidden;
+                background-color: #333;
+            }
+
+            .topnav a {
+                float: left;
+                display: block;
+                color: #f2f2f2;
+                text-align: center;
+                padding: 14px 16px;
+                text-decoration: none;
+                font-size: 17px;
+            }
+
+            .topnav a:hover {
+                background-color: #ddd;
+                color: black;
+            }
+
+            .topnav a.active {
+                background-color: rebeccapurple;
+                color: white;
+            }
             body {
                 padding-top: 40px;
                 padding-bottom: 40px;
@@ -75,6 +101,12 @@
         </form>
         <div class="error"><?php //echo $error;?><?php //echo $username; echo $password;?></div>
 
+        <div class="topnav">
+            <a class="active" href="/index.php">Home</a>
+            <a href="/Forum/new_topic.php">Create a New topic</a>
+            <a href="/Forum/main_forum.php">Go to Main Forum</a>
+            <a href="/Login%20System/login.php">Login</a>
+        </div>
 
 </body>
 <footer>
