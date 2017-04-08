f0090b188887390388a78c2cd15a3232648a8ec0
EStratigakis <e.stratigakis@rgu.ac.uk>
2017-04-08 17:42:27 +0100

    hi

--- Login System/home.php
+++ Login System/home.php
@@ -3,7 +3,54 @@
 <head>
     <meta charset="utf-8">
     <title>Welcome</title>
-    <link rel="stylesheet" href="../style.css" type="text/css" />
+    <link rel="stylesheet" href="/style.css" type="text/css" />
+
+    <style>
+        body, html {
+            height: 100%;
+        }
+
+        .bg {
+            /* The image used */
+            background-image: url("/assets/Plaza_at_The_Robert_Gordon_University_2.jpg");
+
+            /* Full height */
+            height: 100%;
+
+            /* Center and scale the image nicely */
+            background-position: center;
+            background-repeat: no-repeat;
+            background-size: cover;
+        }
+        body {margin:0;}
+
+        .topnav {
+            overflow: hidden;
+            background-color: #333;
+        }
+
+        .topnav a {
+            float: left;
+            display: block;
+            color: #f2f2f2;
+            text-align: center;
+            padding: 14px 16px;
+            text-decoration: none;
+            font-size: 17px;
+        }
+
+        .topnav a:hover {
+            background-color: #ddd;
+            color: black;
+        }
+
+        .topnav a.active {
+            background-color: #4CAF50;
+            color: white;
+        }
+
+    </style>
+
 </head>
 
 <body>
--- index.php
+++ index.php
@@ -3,8 +3,52 @@
 <head>
     <meta charset="utf-8">
     <title>Robert Gordon University</title>
-    <link rel="stylesheet" href="style.css" />
+    <link rel="stylesheet" href="style.css" type="text/css" />
+        <style>
+            body, html {
+                height: 100%;
+            }
 
+            .bg {
+                /* The image used */
+                background-image: url("assets/Plaza_at_The_Robert_Gordon_University_2.jpg");
+
+                /* Full height */
+                height: 100%;
+
+                /* Center and scale the image nicely */
+                background-position: center;
+                background-repeat: no-repeat;
+                background-size: cover;
+            }
+            body {margin:0;}
+
+            .topnav {
+                overflow: hidden;
+                background-color: #333;
+            }
+
+            .topnav a {
+                float: left;
+                display: block;
+                color: #f2f2f2;
+                text-align: center;
+                padding: 14px 16px;
+                text-decoration: none;
+                font-size: 17px;
+            }
+
+            .topnav a:hover {
+                background-color: #ddd;
+                color: black;
+            }
+
+            .topnav a.active {
+                background-color: #4CAF50;
+                color: white;
+            }
+
+        </style>
 </head>
 
 <body>
@@ -17,7 +61,6 @@
 </div>
 
 <div class="bg"></div>
-
 
 </body>
 </html>
