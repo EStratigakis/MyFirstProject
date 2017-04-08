08be25a711848628af91885e426c11e8139c9098
EStratigakis <e.stratigakis@rgu.ac.uk>
2017-04-08 23:05:25 +0100

    hi

--- Login System/login.php
+++ Login System/login.php
@@ -112,9 +112,8 @@
             <label>Username:</label><br>
             <input type="text" name="username" placeholder="username" /><br><br>
             <label>Password:</label><br>
-            <input type="password" name="password" placeholder="password" />
-            <input type="checkbox" name="remember" value="1">Remember Me
-            <br><br>
+            <input type="password" name="password" placeholder="password" /><br><br>
+            <input type="checkbox" name="remember" value="1">Remember Me<br><br>
             <input type="submit" name="submit" value = "login"/>
         </form>
         <div class="error"><?php //echo $error;?><?php //echo $username; echo $password;?></div>
