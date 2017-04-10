9b01e55b6c5e905bc97c779f011d0b99ca13bd27
EStratigakis <e.stratigakis@rgu.ac.uk>
2017-04-10 20:25:40 +0100

    hi

--- Registration/input.php
+++ Registration/input.php
@@ -7,14 +7,14 @@ mysqli_select_db($db,"$db_name")or die("cannot select DB");
 // get data that sent from form
 $fname=$_POST['fname'];
 $lname=$_POST['lname'];
-$number=$_POST['num'];
+$numb=$_POST['num'];
 $mail=$_POST['mail'];
 $deg=$_POST['dg'];
 $year=$_POST['yr'];
 
 $datetime=date("d/m/y h:i:s"); //create date time
 
-$sql="INSERT INTO $tbl_name(fname, lname, num, mail, , dg, yr, datetime)VALUES('$fname', '$lname', '$number', '$mail', '$deg', '$year', '$datetime')";
+$sql="INSERT INTO $tbl_name(fname, lname, num, mail, dg, yr, datetime)VALUES('$fname', '$lname', '$numb', '$mail', '$deg', '$year', '$datetime')";
 $result=mysqli_query($db,$sql);
 
 if($result){
