d9b6baf09f49caf12b4b8e6fbc6a4bfd93f5fcbe
EStratigakis <e.stratigakis@rgu.ac.uk>
2017-04-10 20:20:37 +0100

    hi

---
+++ Registration/input.php
@@ -0,0 +1,28 @@
+<?php
+include_once("dbConnect.php");
+
+$tbl_name="student";
+$db_name="stu"; // Database name
+mysqli_select_db($db,"$db_name")or die("cannot select DB");
+// get data that sent from form
+$fname=$_POST['fname'];
+$lname=$_POST['lname'];
+$number=$_POST['num'];
+$mail=$_POST['mail'];
+$deg=$_POST['dg'];
+$year=$_POST['yr'];
+
+$datetime=date("d/m/y h:i:s"); //create date time
+
+$sql="INSERT INTO $tbl_name(fname, lname, num, mail, , dg, yr, datetime)VALUES('$fname', '$lname', '$number', '$mail', '$deg', '$year', '$datetime')";
+$result=mysqli_query($db,$sql);
+
+if($result){
+    echo "<script type='text/javascript'>alert('submitted successfully!')</script>";
+    echo "Successful<BR>";
+    header('Refresh: 1; URL = http://efstratios.azurewebsites.net');
+}
+else {
+    echo "ERROR";
+}
+?>
