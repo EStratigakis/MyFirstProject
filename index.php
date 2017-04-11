94b6d2ccfbc1ef097b20172d9f4a58075b785e21
EStratigakis <e.stratigakis@rgu.ac.uk>
2017-04-11 12:50:29 +0100

    hi

--- index.php
+++ index.php
@@ -106,12 +106,7 @@
 </div>
 </body>
 <main>
-    <a href="https://www.accuweather.com/en/gr/athens/182536/current-weather/182536" class="aw-widget-legal">
-        <!--
-        By accessing and/or using this code snippet, you agree to AccuWeather’s terms and conditions (in English) which can be found at https://www.accuweather.com/en/free-weather-widgets/terms and AccuWeather’s Privacy Statement (in English) which can be found at https://www.accuweather.com/en/privacy.
-        -->
-    </a><div id="awtd1491687881145" class="aw-widget-36hour"  data-locationkey="" data-unit="f" data-language="en-us" data-useip="true" data-uid="awtd1491687881145" data-editlocation="true"></div>
-    <script type="text/javascript" src="https://oap.accuweather.com/launch.js"></script>
+
 </main>
 <footer>
 
