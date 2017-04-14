9cd45ae2b8e84f0bb01e04cbac58507194c0c560
EStratigakis <e.stratigakis@rgu.ac.uk>
2017-04-14 23:36:56 +0100

    hi

--- Login System/login1.php
+++ Login System/login1.php
@@ -86,9 +86,7 @@ if(empty($_POST["username"]) || empty($_POST["password"]))
         $_SESSION['password'] = $password;
         $result = mysqli_query($db,"SELECT * from users WHERE username = '$username' and password = '$password'");
         if(mysqli_num_rows($result) == 1) {
-            if ($_SESSION['role']=="admin"){
             header("location: home.php");// Redirecting To another Page
-            }
         }
         else{
             echo "Cannot access this page!Contact the administrator!";
