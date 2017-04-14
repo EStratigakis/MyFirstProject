2f5d95d85befc89772659a25e9eddc5c84e57224
EStratigakis <e.stratigakis@rgu.ac.uk>
2017-04-14 23:20:55 +0100

    hi

--- Login System/login1.php
+++ Login System/login1.php
@@ -92,7 +92,6 @@ if(empty($_POST["username"]) || empty($_POST["password"]))
         }
         else{
             echo "Cannot access this page!Contact the administrator!";
-            echo $rows['role'];
         }
     }
     else
