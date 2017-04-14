a15c6600dec97ee865ef28e562dcac4f8590f3e8
EStratigakis <e.stratigakis@rgu.ac.uk>
2017-04-14 23:08:40 +0100

    hi

--- loggedin/loggedout.php
+++ loggedin/loggedout.php
@@ -4,5 +4,5 @@ unset($_SESSION["username"]);
 unset($_SESSION["password"]);
 
 echo "<script type='text/javascript'>alert('Successfully Logged Out')</script>";
-header('URL = ../Login System/login.php');
+header('URL =  http://strato1.azurewebsites.net');
 ?>
