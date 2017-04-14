dc0254666e8fe23073279e34630bedf9b50683f1
EStratigakis <e.stratigakis@rgu.ac.uk>
2017-04-14 23:40:55 +0100

    hi

--- Login System/logout.php
+++ Login System/logout.php
@@ -1,5 +1,5 @@
 <?php
 session_destroy();
 echo "<script type='text/javascript'>alert('Successfully Logged Out')</script>";
-header(' URL = http://strato1.azurewebsites.net');
+header('Refresh: 2; URL = http://strato1.azurewebsites.net');
 ?>
