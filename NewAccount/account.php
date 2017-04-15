0933b32c2cca098ca75c707ab43ab1b855311b46
EStratigakis <e.stratigakis@rgu.ac.uk>
2017-04-15 23:11:11 +0100

    hi

--- NewAccount/account.php
+++ NewAccount/account.php
@@ -13,8 +13,9 @@ $sql1 = "SELECT * FROM $tbl_name WHERE username='$username'"; //SQL Query to che
 $result = mysqli_query($db,$sql1); //Executes Query
 $rows = mysqli_num_rows($result); //Count rows selected (1 if a username/password combo can be found)
 
-if($rows != 1){
+if($rows <> 1){
     $sql="INSERT INTO users(username, password, role, permissions_id) VALUES ('$usr', '$pass','', '$rle')";
+    $result1 = mysqli_query($db,$sql);
     echo "<script type='text/javascript'>alert('User Added!')</script>";
     header('Refresh: 1; URL = http://strato1.azurewebsites.net/NewAccount/account.html');
 }
