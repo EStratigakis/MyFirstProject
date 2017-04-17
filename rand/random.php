002b1317885605549689a21351af87ab49e36054
EStratigakis <e.stratigakis@rgu.ac.uk>
2017-04-17 18:44:09 +0100

    hi

--- rand/random.php
+++ rand/random.php
@@ -1,14 +1,35 @@
 <?php
-    include_once("../dbConnect.php");
 
-    $tbl_name = "student";
-    $db_name = "stu"; // Database name
-    mysqli_select_db($db, "$db_name") or die("cannot select DB");
+include_once("../dbConnect.php");
 
-    $result=mysqli_query($db,"SELECT count(*) as total from student");
-    $data=mysqli_fetch_assoc($result);
+$db_name = "stu"; // Database name
+mysqli_select_db($db, "$db_name") or die("cannot select DB");
 
-    for ($x = 1; $x <= $data['total']; $x++){
-        echo " $x";
+if(empty($_POST["gname"]) || empty($_POST["snum"]))
+    {
+    echo "Both fields are required.";
+    }
+else {
+
+    $gname = $_POST['gname'];
+    $snum = $_POST['snum'];
+    mysqli_select_db($db, 'stu');
+    $sql = "CREATE TABLE $gname(student_id int(4) NOT NULL AUTO_INCREMENT, fname varchar(65) NOT NULL, lname varchar(65)) NOT NULL, num int(7) NOT NULL DEFAULT '', mail varchar(65) NOT NULL, dg varchar(25) NOT NULL, yr VARCHAR (25) NOT NULL
+            PRIMARY KEY (`student_id` ))";
+
+    $result = mysqli_query($db, $sql);
+
+
+    if ($result) {
+
+        $r = "INSERT INTO '$gname'(student_id, fname, lname, num, mail, dg, yr) SELECT DISTINCT * FROM student ORDER BY rand() LIMIT '$snum'";
+
+        $s = mysqli_query($db, $r);
+
+        if ($r) {
+            echo "<script type='text/javascript'>alert('Table has been created and populated')</script>";
+            header('Refresh: 1; URL = http://strato1.azurewebsites.net/rand/rform.html');
+        }
+    }
 }
 ?>
