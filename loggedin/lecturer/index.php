74c09868e431cb85fd23a35e74ff87db8112f6fc
EStratigakis <e.stratigakis@rgu.ac.uk>
2017-04-17 13:41:29 +0100

    hi

--- loggedin/lecturer/index.php
+++ loggedin/lecturer/index.php
@@ -121,21 +121,19 @@ session_start();
     <div class="table-responsive" style="background-color:#eee; height:auto;width:50%;float:left;border: 1px solid black;">
         <div><b>Forum</b></div>
         <p><table class="table" width="90%" border="0" align="center" cellpadding="3" cellspacing="1" bgcolor="#9370db">
-            <thead>
             <tr>
-                <th width="6%" align="center" bgcolor="#E6E6E6"><strong>#</strong></th>
-                <th width="53%" align="center" bgcolor="#E6E6E6"><strong>Topic</strong></th>
-                <th width="15%" align="center" bgcolor="#E6E6E6"><strong>Views</strong></th>
-                <th width="13%" align="center" bgcolor="#E6E6E6"><strong>Replies</strong></th>
-                <th width="13%" align="center" bgcolor="#E6E6E6"><strong>Date/Time</strong></th>
+                <td width="6%" align="center" bgcolor="#E6E6E6"><strong>#</strong></td>
+                <td width="53%" align="center" bgcolor="#E6E6E6"><strong>Topic</strong></td>
+                <td width="15%" align="center" bgcolor="#E6E6E6"><strong>Views</strong></td>
+                <td width="13%" align="center" bgcolor="#E6E6E6"><strong>Replies</strong></td>
+                <td width="13%" align="center" bgcolor="#E6E6E6"><strong>Date/Time</strong></td>
             </tr>
-            </thead>
+
             <?php
 
             // Start looping table row
             while($rows = mysqli_fetch_array($result)){
                 ?>
-            <tbody>
                 <tr>
                     <td bgcolor="#FFFFFF"><?php echo $rows['id']; ?></td>
                     <td bgcolor="#FFFFFF"><a href="/Forum/view_topic.php?id=<?php echo $rows['id']; ?>"><?php echo $rows['topic']; ?></a><BR></td>
@@ -143,7 +141,7 @@ session_start();
                     <td align="center" bgcolor="#FFFFFF"><?php echo $rows['reply']; ?></td>
                     <td align="center" bgcolor="#FFFFFF"><?php echo $rows['datetime']; ?></td>
                 </tr>
-            </tbody>
+
                 <?php
 // Exit looping and close connection
             }
