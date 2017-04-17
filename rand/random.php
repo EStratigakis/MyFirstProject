97251dea5073d1913ee363d667c4d05d3f0de96e
EStratigakis <e.stratigakis@rgu.ac.uk>
2017-04-17 18:51:30 +0100

    hi

--- rand/random.php
+++ rand/random.php
@@ -14,15 +14,14 @@ else {
     $gname = $_POST['gname'];
     $snum = $_POST['snum'];
     mysqli_select_db($db, 'stu');
-    $sql = "CREATE TABLE '$gname'(student_id int(4) NOT NULL AUTO_INCREMENT, fname varchar(65) NOT NULL, lname varchar(65)) NOT NULL, num int(7) NOT NULL DEFAULT '', mail varchar(65) NOT NULL, dg varchar(25) NOT NULL, yr VARCHAR (25) NOT NULL
-            PRIMARY KEY (`student_id` ))";
+    $sql = "CREATE TABLE '$gname'(student_id int(4) NOT NULL AUTO_INCREMENT, fname varchar(65) NOT NULL, lname varchar(65) NOT NULL, num int(7) NOT NULL DEFAULT '', mail varchar(65) NOT NULL, dg varchar(25) NOT NULL, yr VARCHAR (25) NOT NULL;";
 
     $result = mysqli_query($db, $sql);
 
 
     if ($result) {
 
-        $r = "INSERT INTO '$gname'(student_id, fname, lname, num, mail, dg, yr) SELECT DISTINCT * FROM student ORDER BY rand() LIMIT '$snum'";
+        $r = "INSERT INTO '$gname'(student_id, fname, lname, num, mail, dg, yr) SELECT DISTINCT * FROM student ORDER BY rand() LIMIT '$snum';";
 
         $s = mysqli_query($db, $r);
 
