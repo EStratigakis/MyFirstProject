07807a636776f00c75bf596cab5a63eea8229675
EStratigakis <e.stratigakis@rgu.ac.uk>
2017-04-18 21:03:02 +0100

    hi

--- Login System/login.php
+++ Login System/login.php
@@ -8,97 +8,6 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="/style.css" type="text/css" />
     <title>Login!</title>
-    <style>
-        <style>
-        body, html {
-            height: 100%;
-
-            /* Center and scale the image nicely */
-            background-position: center;
-            background-repeat: no-repeat;
-            background-size: cover;
-        }
-        body {margin:0;}
-
-        .topnav {
-            overflow: hidden;
-            background-color: #333;
-        }
-
-        .topnav a {
-            float: left;
-            display: block;
-            color: #f2f2f2;
-            text-align: center;
-            padding: 14px 16px;
-            text-decoration: none;
-            font-size: 17px;
-        }
-
-        .topnav a:hover {
-            background-color: #ddd;
-            color: black;
-        }
-
-        .topnav a.active {
-            background-color: rebeccapurple;
-            color: white;
-        }
-        body {
-
-            padding-bottom: 40px;
-            background-color: white;
-        }
-
-        .form-signin {
-            max-width: 330px;
-            padding: 15px;
-            margin: 0 auto;
-            color: rebeccapurple;
-        }
-
-        .form-signin .form-signin-heading,
-        .form-signin .checkbox {
-            margin-bottom: 10px;
-        }
-
-        .form-signin .checkbox {
-            font-weight: normal;
-        }
-
-        .form-signin .form-control {
-            position: relative;
-            height: auto;
-            -webkit-box-sizing: border-box;
-            -moz-box-sizing: border-box;
-            box-sizing: border-box;
-            padding: 10px;
-            font-size: 16px;
-        }
-
-        .form-signin .form-control:focus {
-            z-index: 2;
-        }
-
-        .form-signin input[type="email"] {
-            margin-bottom: -1px;
-            border-bottom-right-radius: 0;
-            border-bottom-left-radius: 0;
-            border-color:#017572;
-        }
-
-        .form-signin input[type="password"] {
-            margin-bottom: 10px;
-            border-top-left-radius: 0;
-            border-top-right-radius: 0;
-            border-color:#017572;
-        }
-
-        title{
-            text-align: center;
-            color: #017572;
-        }
-    </style>
 </head>
 <body background="/assets/Plaza_at_The_Robert_Gordon_University_2.jpg">
 
@@ -108,15 +17,20 @@
             <a class="navbar-brand" href="/index.html">RGU portal</a>
         </div>
         <ul class="nav navbar-nav">
-            <li class="active"><a href="/index.html">Home</a></li>
-            <li><a href="/Forum/new_topic.php">Create a New topic</a></li>
-            <li><a href="/Forum/main_forum.php">Go to Main Forum</a></li>
-            <li><a href="/Registration/regi.html">Add a student!</a></li>
-            <li><a href="/group/index.php">Student! Selection</a></li>
-            <li><a href="/NewAccount/account.html">Add User Account</a></li>
+            <li class="active"><a href="/loggedin/admin/index.php">Home</a></li>
+            <li class="dropdown">
+                <a class="dropdown-toggle" data-toggle="dropdown" href="#">Menu
+                    <span class="caret"></span></a>
+                <ul class="dropdown-menu">
+                    <li><a href="/Forum/main_forum.php">Forum</a></li>
+                    <li><a href="/Upload/upload.html">Upload</a></li>
+                    <li><a href="/Registration/regi.html">Add Student</a></li>
+                    <li><a href="/NewAccount/account.html">Add User Account</a></li>
+                </ul>
+            </li>
         </ul>
         <ul class="nav navbar-nav navbar-right">
-            <li><a href="/Login%20System/login.php"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+            <li><a href="/Login%20System/login.php"><span class="glyphicon glyphicon-log-out"></span> Login</a></li>
         </ul>
     </div>
 </nav>
@@ -126,13 +40,19 @@
     <h3>Login</h3>
 
         <br><br>
-        <form class = "form-signin" role = "form" method="post" action="login1.php">
-            <label>Username:</label><br>
-            <input type="text" name="username" placeholder="username" /><br><br>
-            <label>Password:</label><br>
-            <input type="password" name="password" placeholder="password" /><br><br>
-            <input type="checkbox" name="remember" value="1">Remember Me<br><br>
-            <input type="submit" name="submit" value = "login"/>
+        <form role = "form" method="post" action="login1.php">
+            <div class="form-group">
+                <label for="username">Username:</label><br>
+                <input class="form-control" type="text" name="username" placeholder="Enter Username" /><br><br>
+            </div>
+            <div class="form-group">
+                <label>Password:</label><br>
+                <input class="form-control" type="password" name="password" placeholder="Enter Password" /><br><br>
+            </div>
+            <div class="checkbox">
+                <label><input type="checkbox" name="remember" value="1">Remember Me</label><br><br>
+            </div>
+                <button type="submit" name="submit" value = "login" class="btn btn-default">Login</button>
         </form>
         <div class="error"><?php //echo $error;?><?php //echo $username; echo $password;?></div>
 
