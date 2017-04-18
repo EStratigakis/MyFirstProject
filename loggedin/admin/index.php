15fb02c919f61b79376952506591d1ecfdbb2a73
EStratigakis <e.stratigakis@rgu.ac.uk>
2017-04-18 20:26:32 +0100

    hi

--- loggedin/admin/index.php
+++ loggedin/admin/index.php
@@ -84,7 +84,7 @@ else {
         <h1 align="center"><?php echo "Hello, " .$_SESSION['username']. "!";?></h1>
     </div>
 
-    <div class="table-responsive" style="background-color:#eee; height:auto;width:60%;float:left;border: 1px solid black;">
+<div class="table-responsive" style="background-color:#eee; height:auto;width:60%;float:left;border: 1px solid black;">
         <div><b>Forum</b></div>
         <p><table width="90%" border="1" align="center" cellpadding="3" cellspacing="1" bgcolor="#9370db">
             <tr>
@@ -117,13 +117,13 @@ else {
                 <td colspan="5" align="right" bgcolor="#E6E6E6"><a href="/Forum/new_topic.php"><strong>Create New Topic</strong> </a></td>
             </tr>
         </table><br><br>
-    </div>
+</div>
 
 
 </div>
-<div class="table-responsive" style="background-color:#fff; height:auto;width:40%;float:right;border: 1px solid black;">
+<div class="table-responsive" style="background-color:#eee; height:auto;width:40%;float:right;border: 1px solid black;">
     <div><b>Group</b></div>
-    <table width="90%" border="1" align="center" cellpadding="3" cellspacing="1" bgcolor="#9370db">
+    <p><table width="90%" border="1" align="center" cellpadding="3" cellspacing="1" bgcolor="#9370db">
         <tr>
             <td width="50%" align="center" bgcolor="#E6E6E6"><strong>Last Name</strong></td>
             <td width="50%" align="center" bgcolor="#E6E6E6"><strong>Matriculation Number</strong></td>
