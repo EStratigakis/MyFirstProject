fd417787fd98d68df4738d79fd9ebafc56c323de
EStratigakis <e.stratigakis@rgu.ac.uk>
2017-04-19 10:00:54 +0100

    hi

--- rand/random.php
+++ rand/random.php
@@ -39,11 +39,9 @@ else {
         for ($x = 1; $x <= $gnum; $x++)
         {
             mysqli_select_db($db, 'stu');
-            $sql = "CREATE TABLE $x(student_id int(4) NOT NULL, fname varchar(65) NOT NULL, lname varchar(65) NOT NULL, num int(7) NOT NULL DEFAULT '0', mail varchar(65) NOT NULL, dg varchar(25) NOT NULL, yr VARCHAR (25) NOT NULL, password VARCHAR (25) NOT NULL, perm_id int(1) NOT NULL)";
+            $sql = "CREATE TABLE `$x`(student_id int(4) NOT NULL, fname varchar(65) NOT NULL, lname varchar(65) NOT NULL, num int(7) NOT NULL DEFAULT '0', mail varchar(65) NOT NULL, dg varchar(25) NOT NULL, yr VARCHAR (25) NOT NULL, password VARCHAR (25) NOT NULL, perm_id int(1) NOT NULL)";
             echo "im here";
             $result = mysqli_query($db, $sql);
-
-            $ruid = rand(1,$data['total']);
 
             if ($result) {
 
