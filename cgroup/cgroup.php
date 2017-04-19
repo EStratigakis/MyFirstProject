612ef37b74a2d44a75ad05a5742bb1daa67a2fa2
EStratigakis <e.stratigakis@rgu.ac.uk>
2017-04-19 12:16:19 +0100

    hi

--- cgroup/cgroup.php
+++ cgroup/cgroup.php
@@ -1,14 +1,6 @@
 <?php
 session_start();
-if ($_SESSION['permissions_id'] != 3)
-{
-    header("Refresh: 3; url= /index.html");
-    echo '<h3>ACCESS DENIED - YOU DO NOT HAVE PERMISSIONS TO ACCESS THIS PAGE</h3>';
-    echo 'You will be redirected in 3 seconds';
-    session_destroy();
-    exit();
-}
-else {
+
     include_once("../dbConnect.php");
 
     $tbl_name = "groups";
@@ -38,6 +30,6 @@ else {
             echo "<script type='text/javascript'>alert('Table Name Created!')</script>";
             header('Refresh: 1; URL = http://strato1.azurewebsites.net/cgroup/cgroup.html');
         }
-    }
+
 }
 ?>
