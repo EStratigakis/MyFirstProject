7ce256e2aefecf95f48a02590494a23641dce053
EStratigakis <e.stratigakis@rgu.ac.uk>
2017-04-19 05:32:26 +0100

    hi

--- Login System/stulogin.php
+++ Login System/stulogin.php
@@ -37,7 +37,7 @@ if(empty($_POST["num"]) || empty($_POST["password"]))
         $_SESSION['password'] = $password;
         $result1 = mysqli_query($db, "SELECT * from students WHERE num = $username and password = '$password'");
         if (mysqli_num_rows($result1) == 1) {
-            switch ($_SESSION['permissions_id']) {
+            switch ($_SESSION['perm_id']) {
                 default:
                     echo 'You do not have enough privileges';
                     break;
