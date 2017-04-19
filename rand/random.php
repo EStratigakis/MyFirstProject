323d20dd38b984a41d00f10dad4c9d11766edf34
EStratigakis <e.stratigakis@rgu.ac.uk>
2017-04-19 09:44:38 +0100

    hi

--- rand/random.php
+++ rand/random.php
@@ -34,6 +34,7 @@ else {
 
         for ($x = 1; $x <= $gnum; $x++)
         {
+            mysqli_select_db($db, 'stu');
             $sql = "CREATE TABLE $x(student_id int(4) NOT NULL, fname varchar(65) NOT NULL, lname varchar(65) NOT NULL, num int(7) NOT NULL DEFAULT '0', mail varchar(65) NOT NULL, dg varchar(25) NOT NULL, yr VARCHAR (25) NOT NULL, password VARCHAR (25) NOT NULL, perm_id int(1) NOT NULL)";
             echo "im here";
             $result = mysqli_query($db, $sql);
@@ -42,7 +43,7 @@ else {
 
             if ($result) {
 
-                $r = "INSERT INTO $x(student_id, fname, lname, num, mail, dg, yr, password, perm_id) SELECT * FROM student WHERE student_id = $ruid LIMIT $lmt;";
+                $r = "INSERT INTO $x(student_id, fname, lname, num, mail, dg, yr, password, perm_id) SELECT * FROM student WHERE student_id = $ruid LIMIT 0,$lmt;";
                 mysqli_select_db($db, "$db_name") or die("cannot select DB");
                 $s = mysqli_query($db, $r);
                 echo "no im here";
