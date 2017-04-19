6c631953f6c72c575746002de0f352a000b2cb24
EStratigakis <e.stratigakis@rgu.ac.uk>
2017-04-19 08:24:36 +0100

    hi

--- loggedin/lecturer/index.php
+++ loggedin/lecturer/index.php
@@ -39,7 +39,7 @@ else {
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-    <title>Lecturer Login!</title>
+    <title>Lecturer Home Page</title>
     <link rel="stylesheet" href="/style.css" type="text/css" />
     <style>
         footer {
--- rand/random.php
+++ rand/random.php
@@ -14,27 +14,41 @@ else {
     $db_name = "stu"; // Database name
     mysqli_select_db($db, "$db_name") or die("cannot select DB");
 
-    if (empty($_POST["gname"]) || empty($_POST["snum"])) {
-        echo "<script type='text/javascript'>alert('Both fields are required!')</script>";
+    if (empty($_POST["gnum"])) {
+        echo "<script type='text/javascript'>alert('Thr required field is not filled!')</script>";
         header('Refresh: 1; URL = http://strato1.azurewebsites.net');
     } else {
-        $gname = $_POST['gname'];
-        $snum = $_POST['snum'];
+        $gnum = $_POST['gnum'];
+
         mysqli_select_db($db, 'stu');
-        $sql = "CREATE TABLE `$gname`(student_id int(4) NOT NULL, fname varchar(65) NOT NULL, lname varchar(65) NOT NULL, num int(7) NOT NULL DEFAULT '0', mail varchar(65) NOT NULL, dg varchar(25) NOT NULL, yr VARCHAR (25) NOT NULL, password VARCHAR (25) NOT NULL, perm_id int(1) NOT NULL DEFAULT '2')";
 
-        $result = mysqli_query($db, $sql);
+        $w = "SELECT COUNT(*) AS total FROM student;";
+        $res = mysqli_query($db, $w);
+        $data = mysqli_fetch_assoc($res);
 
+        $snum = $data['total'] / $gnum;
 
-        if ($result) {
+        $lmt = round($snum, PHP_ROUND_HALF_UP);
 
-            $r = "INSERT INTO `$gname`(student_id, fname, lname, num, mail, dg, yr, password, perm_id) SELECT DISTINCT * FROM student ORDER BY rand() LIMIT $snum;";
-            mysqli_select_db($db, "$db_name") or die("cannot select DB");
-            $s = mysqli_query($db, $r);
+        $x = 1;
 
-            if ($r) {
-                echo "<script type='text/javascript'>alert('Table has been created and populated')</script>";
-                ?><script type="text/javascript">window.history.go(-2);</script><?php
+        for ($x = 1; $x <= $gnum; $x++) {
+            $sql = "CREATE TABLE $x(student_id int(4) NOT NULL, fname varchar(65) NOT NULL, lname varchar(65) NOT NULL, num int(7) NOT NULL DEFAULT '0', mail varchar(65) NOT NULL, dg varchar(25) NOT NULL, yr VARCHAR (25) NOT NULL, password VARCHAR (25) NOT NULL, perm_id int(1) NOT NULL DEFAULT '2')";
+
+            $result = mysqli_query($db, $sql);
+
+            $ruid = rand(1, $data['total']);
+
+            if ($result) {
+
+                $r = "INSERT INTO $x(student_id, fname, lname, num, mail, dg, yr, password, perm_id) SELECT * FROM student WHERE student_id = $ruid LIMIT $lmt';'";
+                mysqli_select_db($db, "$db_name") or die("cannot select DB");
+                $s = mysqli_query($db, $r);
+                if ($r) {
+                    echo "<script type='text/javascript'>alert('Table has been created and populated')</script>";
+                    ?>
+                    <script type="text/javascript">window.history.go(-2);</script><?php
+                }
             }
         }
     }
