e683f22603c668edfa650fb48698c59ad2a2ec13
EStratigakis <e.stratigakis@rgu.ac.uk>
2017-04-19 09:41:00 +0100

    hi

--- rand/random.php
+++ rand/random.php
@@ -35,7 +35,7 @@ else {
         for ($x = 1; $x <= $gnum; $x++)
         {
             $sql = "CREATE TABLE $x(student_id int(4) NOT NULL, fname varchar(65) NOT NULL, lname varchar(65) NOT NULL, num int(7) NOT NULL DEFAULT '0', mail varchar(65) NOT NULL, dg varchar(25) NOT NULL, yr VARCHAR (25) NOT NULL, password VARCHAR (25) NOT NULL, perm_id int(1) NOT NULL)";
-
+            echo "im here";
             $result = mysqli_query($db, $sql);
 
             $ruid = rand(1, $data['total']);
@@ -45,6 +45,7 @@ else {
                 $r = "INSERT INTO $x(student_id, fname, lname, num, mail, dg, yr, password, perm_id) SELECT * FROM student WHERE student_id = $ruid LIMIT $lmt;";
                 mysqli_select_db($db, "$db_name") or die("cannot select DB");
                 $s = mysqli_query($db, $r);
+                echo "no im here";
                 if ($r) {
                     echo "<script type='text/javascript'>alert('Table has been created and populated')</script>";
                     ?>
