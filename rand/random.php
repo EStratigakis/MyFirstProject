4f548d95540d4323215413507fa70a682b106244
EStratigakis <e.stratigakis@rgu.ac.uk>
2017-04-19 10:23:01 +0100

    hi

--- rand/random.php
+++ rand/random.php
@@ -20,9 +20,6 @@ else {
     } else {
 
         $gnum = $_POST['gnum'];
-        $rnum = $_POST['rnum'];
-        $r1num = $_POST['r1num'];
-        $r2num = $_POST['r2num'];
 
         mysqli_select_db($db, 'stu');
 
@@ -40,15 +37,21 @@ else {
         {
             mysqli_select_db($db, 'stu');
             $sql = "CREATE TABLE `$x`(student_id int(4) NOT NULL, fname varchar(65) NOT NULL, lname varchar(65) NOT NULL, num int(7) NOT NULL DEFAULT '0', mail varchar(65) NOT NULL, dg varchar(25) NOT NULL, yr VARCHAR (25) NOT NULL, password VARCHAR (25) NOT NULL, perm_id int(1) NOT NULL)";
-            echo "im here";
             $result = mysqli_query($db, $sql);
 
             if ($result) {
+                    if ($x >= 2) {
+                        $t = "SELECT uid FROM student;";
+                        $res1 = mysqli_query($db,$t);
 
-                $r = "INSERT INTO `$x`(student_id, fname, lname, num, mail, dg, yr, password, perm_id) SELECT DISTINCT * FROM student LIMIT $lmt;";
-                mysqli_select_db($db, "$db_name") or die("cannot select DB");
-                $s = mysqli_query($db, $r);
-                echo "no im here";
+                        $r = "INSERT INTO `$x`(student_id, fname, lname, num, mail, dg, yr, password, perm_id) SELECT DISTINCT * FROM student ORDER BY rand() LIMIT $lmt;";
+                        mysqli_select_db($db, "$db_name") or die("cannot select DB");
+                        $s = mysqli_query($db, $r);
+                    }else {
+                        $r = "INSERT INTO `$x`(student_id, fname, lname, num, mail, dg, yr, password, perm_id) SELECT DISTINCT * FROM student ORDER BY rand() LIMIT $lmt;";
+                        mysqli_select_db($db, "$db_name") or die("cannot select DB");
+                        $s = mysqli_query($db, $r);
+                    }
                 if ($r) {
                     echo "<script type='text/javascript'>alert('Table has been created and populated')</script>";
                     ?>
