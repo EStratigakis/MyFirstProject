1e5217059e12462be148bf5b15a0c1cedaaf46af
EStratigakis <e.stratigakis@rgu.ac.uk>
2017-04-20 14:26:45 +0100

    hi

--- rand/random.php
+++ rand/random.php
@@ -11,7 +11,7 @@ session_start();
         mysqli_select_db($db, 'stu') or die("cannot select DB");
         $sql = "CREATE TABLE $gname(student_id int(4) NOT NULL, fname varchar(65) NOT NULL, lname varchar(65) NOT NULL, num int(7) NOT NULL DEFAULT '0', mail varchar(65) NOT NULL, dg varchar(25) NOT NULL, yr VARCHAR (25) NOT NULL, password VARCHAR (25) NOT NULL, perm_id int(1) NOT NULL)";
         $result = mysqli_query($db, $sql);
-echo "im here11111";
+
     if ($result) {
         $r = "INSERT INTO $gname(student_id, fname, lname, num, mail, dg, yr, password, perm_id) SELECT * FROM student WHERE dg = '$deg'";
         $s = mysqli_query($db, $r);
