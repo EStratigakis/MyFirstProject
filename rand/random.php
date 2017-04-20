e235301a5fcc4d28dda80becd83c531586b6193e
EStratigakis <e.stratigakis@rgu.ac.uk>
2017-04-20 14:00:46 +0100

    hi

--- rand/random.php
+++ rand/random.php
@@ -5,37 +5,20 @@ session_start();
     $db_name = "stu"; // Database name
     mysqli_select_db($db, "$db_name") or die("cannot select DB");
 
-         $gnum = $_POST['gnum'];
+        $gname = $_POST['gname'];
+        $deg = $_POST['deg'];
 
-        mysqli_select_db($db, 'stu');
+        $sql = "CREATE TABLE `$gname`(student_id int(4) NOT NULL, fname varchar(65) NOT NULL, lname varchar(65) NOT NULL, num int(7) NOT NULL DEFAULT '0', mail varchar(65) NOT NULL, dg varchar(25) NOT NULL, yr VARCHAR (25) NOT NULL, password VARCHAR (25) NOT NULL, perm_id int(1) NOT NULL)";
+        $result = mysqli_query($db, $sql);
 
-        $w = "SELECT student_id FROM student;";
-        $res = mysqli_query($db, $w);
+    if ($result) {
+        $r = "INSERT INTO `$gname`(student_id, fname, lname, num, mail, dg, yr, password, perm_id) SELECT dg FROM student WHERE dg = '$deg'";
+        $s = mysqli_query($db, $r);
 
-        $e = "SELECT COUNT(*) AS total FROM groups;";
-        $res1 = mysqli_query($db, $e);
-        $data = mysqli_fetch_assoc($res1);
-
-        $gcount = 0;
-
-        while ($res)
-        {
-            if ($gcount <= $data['total'])
-            {
-                $t = "INSERT INTO assign_group(student_id,group_id) VALUES ($row,$gcount);";
-                $res2 = mysqli_query($db,$t);
-
-                if ($res2) {?><script type="text/javascript">window.history.go(-2);</script><?php}
-                else{echo "<script type='text/javascript'>alert('Error Importing Values!')</script>";}
-            }
-            else
-            {
-                $gcount = 1;
-                $q = "INSERT INTO assign_group(student_id,group_id) VALUES ($row,$gcount);";
-                $res3 = mysqli_query($db,$q);
-
-                if ($res3) {?><script type="text/javascript">window.history.go(-2);</script><?php}
-                else{echo "<script type='text/javascript'>alert('Error Importing Values!')</script>";}
-            }
+        if ($r) {
+            echo "<script type='text/javascript'>alert('Table has been created and populated')</script>";
+            ?>
+            <script type="text/javascript">window.history.go(-2);</script><?php
         }
+    }
 ?>
