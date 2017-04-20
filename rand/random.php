bfbe92e00b77d38765ef2a9fe6b2be28292958f6
EStratigakis <e.stratigakis@rgu.ac.uk>
2017-04-20 14:34:06 +0100

    hi

--- rand/random.php
+++ rand/random.php
@@ -8,7 +8,7 @@ session_start();
         $grname = $_POST['grname'];
         $deg = $_POST['deg'];
 
-        mysqli_select_db($db, 'stu') or die("cannot select DB");
+        mysqli_select_db($db, $db_name) or die("cannot select DB");
         $sql = "CREATE TABLE '$grname'(student_id int(4) NOT NULL, fname varchar(65) NOT NULL, lname varchar(65) NOT NULL, num int(7) NOT NULL DEFAULT '0', mail varchar(65) NOT NULL, dg varchar(25) NOT NULL, yr VARCHAR (25) NOT NULL, password VARCHAR (25) NOT NULL, perm_id int(1) NOT NULL)";
         $result = mysqli_query($db, $sql);
 
