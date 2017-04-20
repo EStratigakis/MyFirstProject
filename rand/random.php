24a99d104af2a17a05559facd409941730a9abcd
EStratigakis <e.stratigakis@rgu.ac.uk>
2017-04-20 15:55:19 +0100

    hi

--- rand/random.php
+++ rand/random.php
@@ -12,11 +12,12 @@ session_start();
         $sql = "CREATE TABLE ".$grname."(student_id int(4) NOT NULL, fname varchar(65) NOT NULL, lname varchar(65) NOT NULL, num int(7) NOT NULL DEFAULT '0', mail varchar(65) NOT NULL, dg varchar(25) NOT NULL, yr VARCHAR (25) NOT NULL, password VARCHAR (250) NOT NULL, perm_id int(1) NOT NULL);";
         $result = mysqli_query($db, $sql);
 
-    if ($result) {
+    if ($result)
+    {
         mysqli_select_db($db, $db_name) or die("cannot select DB");
         $r = "INSERT INTO ".$grname."(student_id, fname, lname, num, mail, dg, yr, password, perm_id) SELECT * FROM student WHERE dg = '$deg'";
         $s = mysqli_query($db, $r);
-echo "im here";
+
         if ($s) {
             echo "<script type='text/javascript'>alert('Table has been created and populated')</script>";
             ?>
