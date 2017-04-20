de5493744564948fce2a721a22285979c6b7e9b6
EStratigakis <e.stratigakis@rgu.ac.uk>
2017-04-20 14:55:22 +0100

    hi

--- rand/random.php
+++ rand/random.php
@@ -23,4 +23,9 @@ echo "im here";
             <script type="text/javascript">window.history.go(-2);</script><?php
         }
     }
+    else{
+        echo "<script type='text/javascript'>alert('Table already exists')</script>";
+        ?>
+        <script type="text/javascript">window.history.go(-2);</script><?php
+    }
 ?>
